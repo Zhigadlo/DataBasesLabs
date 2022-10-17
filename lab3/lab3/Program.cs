ad00747242de6676bb5f9afbc79bfef62f9b3920
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-10-17 13:20:43 +0300

    Add connection string and /info page

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -1,6 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.GetConnectionString("DefaultConnection");
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
+app.Map("/info", Info);
+app.Run(async (context) => await context.Response.WriteAsync("It's lab nubmer 3"));
 app.Run();
+
+
+void Info(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync($"<h1>User: {context.User}</h1>");
+    });
+
+}
