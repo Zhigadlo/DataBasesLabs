8fbf572b0d95e506b0ca56b53206a3217a719b16
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-11-28 16:36:04 +0300

    Create ingridients controller

---
+++ lab6/lab6/Controllers/IngridientsController.cs
@@ -0,0 +1,68 @@
+﻿using lab6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab6.Controllers
+{
+    [Route("api/[controller]")]
+    public class IngridientsController : Controller
+    {
+        private CafeContext _context;
+
+        public IngridientsController(CafeContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Ingridient> Get()
+        {
+            return _context.Ingridients.AsEnumerable();
+        }
+        [HttpGet("{id}")]
+        public Ingridient? Get(int id)
+        {
+            return _context.Ingridients.FirstOrDefault(i => i.Id == id);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Ingridient ingridient)
+        {
+            if (ingridient != null)
+            {
+                _context.Ingridients.Add(ingridient);
+                _context.SaveChanges();
+                return Ok();
+            }
+            else
+                return BadRequest();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Ingridient? ingridient = _context.Ingridients.FirstOrDefault(i => i.Id == id);
+            if (ingridient == null)
+                return NotFound();
+            else
+            {
+                _context.Ingridients.Remove(ingridient);
+                _context.SaveChanges();
+                return Ok();
+            }
+        }
+
+        [HttpPut]
+        public IActionResult Update(int id, Ingridient ingridient)
+        {
+            if (_context.Ingridients.Count(i => i.Id == id) == 0)
+                return NotFound();
+            else
+            {
+                ingridient.Id = id;
+                _context.Ingridients.Update(ingridient);
+                _context.SaveChanges();
+                return Ok();
+            } 
+        }
+    }
+}
