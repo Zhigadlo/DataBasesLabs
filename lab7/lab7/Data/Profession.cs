e3e0818ee87d93553e6128a0bc093f039326220b
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-12-17 23:18:21 +0300

    Create pages for profession creating and updating

--- lab7/lab7/Data/Profession.cs
+++ lab7/lab7/Data/Profession.cs
@@ -1,9 +1,12 @@
-﻿namespace lab7.Data;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lab7.Data;
 
 public partial class Profession
 {
     public int Id { get; set; }
-
+    [Required]
+    [StringLength(20, ErrorMessage = "Name must be not empty or not too short")]
     public string? Name { get; set; }
 
     public virtual ICollection<Employee> Employees { get; } = new List<Employee>();
