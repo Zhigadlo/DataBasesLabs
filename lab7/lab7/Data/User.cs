aff49b420b07628c0ba58cc9e8d4821b4c39f8a2
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-12-18 00:10:41 +0300

    Create user service

--- lab7/lab7/Data/User.cs
+++ lab7/lab7/Data/User.cs
@@ -6,7 +6,7 @@ namespace lab7.Data
     {
         public int Id { get; set; }
         [Required]
-        [StringLength(20, MinimumLength = 8, ErrorMessage = "Login must contaib from 8 to 20 letters")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Login must contain from 8 to 20 letters")]
         public string Login { get; set; }
         [Required]
         public string PasswordHash { get; set; }
---
+++ lab7/lab7/Services/UserService.cs
@@ -0,0 +1,85 @@
+﻿using lab7.Data;
+using lab7.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lab7.Services
+{
+    public class UserService
+    {
+        private UsersContext _context;
+
+        public UserService(UsersContext context)
+        {
+            _context = context;
+        }
+        
+        public string Token(string email, string password)
+        {
+            var identity = GetIdentity(email, password);
+            if (identity == null)
+            {
+                return "Invalid login or password.";
+            }
+
+            var now = DateTime.UtcNow;
+
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    notBefore: now,
+                    claims: identity.Claims,
+                    expires: now.Add(TimeSpan.FromMinutes(AuthOptions.LIFETIME)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+            var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            var response = new
+            {
+                access_token = encodedJwt
+            };
+
+            return response.ToString();
+        }
+        private ClaimsIdentity GetIdentity(string login, string password)
+        {
+            User? person = FindUser(login, password);
+            if (person != null)
+            {
+                var claims = new List<Claim>
+                {
+                    new Claim(ClaimsIdentity.DefaultNameClaimType, person.Login)
+                };
+                ClaimsIdentity claimsIdentity =
+                new ClaimsIdentity(claims, "Token", ClaimsIdentity.DefaultNameClaimType,
+                    ClaimsIdentity.DefaultRoleClaimType);
+                return claimsIdentity;
+            }
+
+            return null;
+        }
+
+        private User? FindUser(string login, string password)
+        {
+            return _context.Users.FirstOrDefault(u => u.Login == login && 
+                                                      u.PasswordHash == PasswordToHash(password));
+        }
+
+        private string PasswordToHash(string password)
+        {
+            using (var hashAlg = MD5.Create())
+            {
+                byte[] hash = hashAlg.ComputeHash(Encoding.UTF8.GetBytes(password));
+                var builder = new StringBuilder(hash.Length * 2);
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    builder.Append(hash[i].ToString("X2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+    }
+}
