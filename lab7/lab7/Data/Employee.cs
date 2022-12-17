b1f7af534108573dc6c4c1fae3905cb5f9bb9441
Zhigadlo <74291930+Zhigadlo@users.noreply.github.com>
2022-12-17 19:29:49 +0300

    Add validations to employee forms

--- lab7/lab7/Data/Employee.cs
+++ lab7/lab7/Data/Employee.cs
@@ -1,19 +1,27 @@
-﻿namespace lab7.Data;
+﻿using Newtonsoft.Json.Serialization;
+using System.ComponentModel.DataAnnotations;
+
+namespace lab7.Data;
 
 public partial class Employee
 {
     public int Id { get; set; }
-
+    [Required]
+    [StringLength(20, ErrorMessage = "First name must be not empty or not too short")]
     public string FirstName { get; set; } = null!;
-
+    [Required]
+    [StringLength(20, ErrorMessage = "Last name must be not empty or not too short")]
     public string LastName { get; set; } = null!;
-
+    [Required]
+    [StringLength(20, ErrorMessage = "Middle name must be not empty or not too short")]
     public string? MiddleName { get; set; }
-
-    public int? Age { get; set; }
-
+    [Required]
+    [Range(18, 65, ErrorMessage = "Age must be between 18 and 65")]
+    public int Age { get; set; }
+    [Required]
+    [StringLength(50, ErrorMessage = "Education must be not empty or not too short")]
     public string? Education { get; set; }
-
+    [Required]
     public int ProfessionId { get; set; }
 
     public virtual ICollection<Order> Orders { get; } = new List<Order>();
---
+++ lab7/lab7/Models/EditEmployeeModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lab7.Models
+{
+    public class EditEmployeeModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "First name must be not empty")]
+        public string FirstName { get; set; } = null!;
+        [Required(ErrorMessage = "Last name must be not empty")]
+        public string LastName { get; set; } = null!;
+        [Required(ErrorMessage = "Middle name must be not empty")]
+        public string MiddleName { get; set; }
+        [Required]
+        [Range(18, 65, ErrorMessage = "Age must be between 18 and 65")]
+        public int Age { get; set; }
+        [Required(ErrorMessage = "Education must be not empty")]
+        public string Education { get; set; }
+        [Required]
+        public int ProfessionId { get; set; }
+    }
+}
